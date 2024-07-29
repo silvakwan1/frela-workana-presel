fd519f942d13853f20bc004db7c7502c91e3c321
silvakwan1 <kauans003@gmail.com>
2024-07-28 23:59:51 -0300

    ADICIONEI UM AUDIO E MECHI NA ROLETA

--- src/js/spin.js
+++ src/js/spin.js
@@ -1,11 +1,16 @@
 function spin() {
   const popUp = document.querySelector(".pop-up");
   const roleta = document.querySelector(".roleta-image");
+  const audio = document.querySelector(".audio");
 
   roleta.classList.add("spin");
+  audio.play();
   setInterval(() => {
     roleta.classList.add("ganhador");
   }, 2500);
+  setInterval(() => {
+    audio.pause();
+  }, 5000);
   setInterval(() => {
     popUp.classList.add("pop-up-active");
   }, 7500);
