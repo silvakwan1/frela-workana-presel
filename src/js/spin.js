9f99cb3a985e0192622f996994610e0f7e572550
silvakwan1 <kauans003@gmail.com>
2024-07-28 21:42:16 -0300

    finix

--- src/js/spin.js
+++ src/js/spin.js
@@ -1,3 +1,12 @@
 function spin() {
-  console.log("ola");
+  const popUp = document.querySelector(".pop-up");
+  const roleta = document.querySelector(".roleta-image");
+
+  roleta.classList.add("spin");
+  setInterval(() => {
+    roleta.classList.add("ganhador");
+  }, 2500);
+  setInterval(() => {
+    popUp.classList.add("pop-up-active");
+  }, 7500);
 }
